79d65eee36fbdda251d1ca99b2e39f150f29fbec
Juan Jose Jimenez Monsalve <jjsanchez689@gmail.com>
2024-09-25 11:02:52 -0500

    fix: baseUrl api

--- src/infraestructure/network/apis.ts
+++ src/infraestructure/network/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {Alert} from 'react-native';
 
 const api = axios.create({
-  baseURL: 'https://fakeasdasdasdstoreapi.com',
+  baseURL: 'https://fakestoreapi.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
